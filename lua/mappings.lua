69e73d4d03fd516a5b61ac8d27c7b4d892b92584
Hải Long <hailong2004ptcnn@gmail.com>
2025-04-16 10:09:55 +0100

    remove bugged shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,7 +15,6 @@ map("", "<right>", "<nop>")
 
 map({ "n", "i" }, "<C-Tab>", "<Cmd>bnext<CR>")
 map({ "n", "i" }, "<C-S-Tab>", "<Cmd>bprevious<CR>")
-map("n", "<leader>v", "<nop>")
 
 -- map('n', 'gh', vim.lsp.buf.hover, {})
 -- map('n', 'gd', vim.lsp.buf.definition, {})
