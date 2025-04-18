31737cdcd875b6e10bfd056606b9ac50c6ed15b0
Hải Long <hailong2004ptcnn@gmail.com>
2025-04-18 11:01:40 +0700

    add dap

--- lua/options.lua
+++ lua/options.lua
@@ -8,10 +8,4 @@ local o = vim.o
 
 o.cursorlineopt = "both" -- to enable cursorline!
 o.swapfile = false
-o.shell = "pwsh.exe"
-
-vim.cmd "set clipboard+=unnamedplus"
-vim.cmd "set expandtab"
-vim.cmd "set tabstop=2"
-vim.cmd "set softtabstop=2"
-vim.cmd "set shiftwidth=2"
+o.shell = "pwsh"
